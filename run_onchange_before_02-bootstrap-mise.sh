5739ecd0dc98e61dc645d02f2d7c18e98491e0c5
Joke de Buhr <joke@xckk.de>
2026-04-03 16:00:40 +0200

    chore: mise config

---
+++ run_onchange_before_02-bootstrap-mise.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# mise already installed
+command -v mise >/dev/null 2>&1 && exit 0
+
+echo "###################### start bootstrapping mise ######################"
+set -ex
+
+wget="$(command -v wget 2>/dev/null)"
+curl="$(command -v curl 2>/dev/null)"
+
+export \
+  MISE_INSTALL_HELP=0 \
+  MISE_EXPERIMENTAL=true \
+  MISE_INSTALL_PATH="$HOME/.local/bin/mise" \
+  PATH="$HOME/.local/bin:$PATH"
+
+mkdir -p "$HOME/.local/bin"
+
+if [ -n "$curl" ]; then
+  "$curl" -sfL https://mise.run | sh
+elif [ -n "$wget" ]; then
+  "$wget" -qO- https://mise.run | sh
+else
+  echo "Neither curl nor wget detected" >&2
+fi
+
+"$MISE_INSTALL_PATH" use -g github:jdx/mise@latest
+ln -sf ../share/mise/installs/github-jdx-mise/latest/bin/mise "$HOME/.local/bin/mise"
+
+set +ex
+echo "###################### done bootstrapping mise ######################"
